0306d7a02b7d3161e5eaf420983cbdec2d55cbcd
Schmidbauer Florian <florian.schmidbauer@htlwy.at>
2023-09-28 18:44:15 +0200

    class ipLocator added for output

--- script_IPLocator.js
+++ script_IPLocator.js
@@ -6,7 +6,7 @@ let xmlHttp = new XMLHttpRequest();
             let output = JSON.parse(xmlHttp.responseText);
             document.getElementById('output').innerHTML = "Status: " + output.status + "<br> countryCode: " + output.countryCode + "<br> lat:" 
             + output.lat + "<br> lon:" + output.lon + "<br> timezone: " + output.timezone + "<br> isp:" + output.isp + "<br> org:" + output.org;
-            document.getElementById("output2").innerHTML = "<a href='https://www.google.com/maps/search/?api=1&query="+output.lat+","+output.lon+"'>Location on Google Maps</a>"
+            document.getElementById("output2").innerHTML = "<a class='ipLocator' href='https://www.google.com/maps/search/?api=1&query="+output.lat+","+output.lon+"'>Location on Google Maps</a>"
         }
     }
 
