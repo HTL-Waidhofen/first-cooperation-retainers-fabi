890fe974acfdbee294084500b0cc07c85e8deb5b
Schmidbauer Florian <florian.schmidbauer@htlwy.at>
2023-10-05 08:53:18 +0200

    added whole recipe output

--- recipes.js
+++ recipes.js
@@ -10,7 +10,7 @@ xmlHttp.open('GET', 'recipes.json', true);
 xmlHttp.send(null);
 
 function getRecipe(){
-    document.getElementById('page').innerHTML = '<h1 id="name"></h1><div id="description"></div><div class="content"><h2>Ingredients:</h2><br/><ul id="ingredients"></ul></div><div id="output"></div>'
+    document.getElementById('page').innerHTML = '<h1 id="name"></h1><div id="description"></div><div class="content"><h2>Ingredients:</h2><br/><ul id="ingredients"></ul><br/><h2>Method:</h2><ul id="method"></ul></div><div id="output"></div><footer><p>To the recipe</p><a id="recipe" href="">Recipe</a><p id="author"></p></footer>'
 
     let ran = Math.floor(Math.random() * 1618)
     o = JSON.parse(xmlHttp.responseText);
@@ -22,4 +22,9 @@ function getRecipe(){
     for(let i = 0; i < o[ran].Ingredients.length; i++){
         document.getElementById('ingredients').innerHTML += `<li>${o[ran].Ingredients[i]}</li>`;
     }
+    for(let i = 0; i < o[ran].Method.length; i++){
+        document.getElementById('method').innerHTML += `<li>${o[ran].Method[i]}</li>`;
+    }
+    document.getElementById('recipe').href = o[ran].url;
+    document.getElementById('author').innerHTML = "by " + o[ran].Author;
 }
