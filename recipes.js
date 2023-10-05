ba00288a687579a6781f553b030e2234004a3ce5
Schmidbauer Florian <florian.schmidbauer@htlwy.at>
2023-10-05 08:36:48 +0200

    added output for name, decription, ingredients

--- recipes.js
+++ recipes.js
@@ -10,6 +10,14 @@ xmlHttp.open('GET', 'recipes.json', true);
 xmlHttp.send(null);
 
 function getRecipe(){
+    let ran = Math.floor(Math.random() * 1618)
     o = JSON.parse(xmlHttp.responseText);
-    document.getElementById('output').innerHTML = o[Math.floor(Math.random() * 1618)].Name;
+
+    document.getElementById('ingredients').innerHTML = "";
+
+    document.getElementById('name').innerHTML = o[ran].Name;
+    document.getElementById('description').innerHTML = o[ran].Description;
+    for(let i = 0; i < o[ran].Ingredients.length; i++){
+        document.getElementById('ingredients').innerHTML += `<li>${o[ran].Ingredients[i]}</li>`;
+    }
 }
